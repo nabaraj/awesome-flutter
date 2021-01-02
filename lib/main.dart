e44c912be663ef4a5902bc9dad486ca70cf474c0
Nabaraj Saha <nabara2k@gmail.com>
2021-01-02 09:55:37 +0530

    [dev#ch2] completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // return Container(color: Colors.amber, child: Text("Nabaraj Saha"));
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Container(child: Center(child: Text("Hi Flutter"))),
-    );
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              color: Colors.black,
+              width: 200,
+              height: 320,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                        width: 100, height: 100, color: Colors.green[200]),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow[200],
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
